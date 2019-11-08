6d1052322fc8fe54fbcfc28bd67cc31a2477e8ec
Henning Klasén <henning@fastmail.com>
2019-11-08 10:23:57 +0100

    Add some more tests

--- test/Allotter.spec.ts
+++ test/Allotter.spec.ts
@@ -4,22 +4,44 @@ import { expect } from 'chai';
 import chaiString = require('chai-string');
 use(chaiString);
 
-
-import { TestUtils as TU } from './TestUtils';
 import { Allotter } from '../src/Allotter';
 
 const defaultDateFormat = 'YYYY-MM-DD HH:mm:ss';
 
 describe('Allotter errors', () => {
-    describe('with non-integer parts input', () => {
+    describe('with decimal parts input', () => {
         it('should throw an error', () => {
                 expect(() => { Allotter.allot(1.2341, 1) }).to.throw();
         });
     });
 
-    describe('with non-integer slots input', () => {
+    describe('with decimal slots input', () => {
         it('should throw an error', () => {
                 expect(() => { Allotter.allot(1, 1.2341) }).to.throw();
+        });
+    });
+
+    describe('with zero as parts input', () => {
+        it('should throw an error', () => {
+                expect(() => { Allotter.allot(0, 1) }).to.throw();
+        });
+    });
+
+    describe('with zero as slots input', () => {
+        it('should throw an error', () => {
+                expect(() => { Allotter.allot(1, 0) }).to.throw();
+        });
+    });
+
+    describe('with less than one as parts input', () => {
+        it('should throw an error', () => {
+            expect(() => { Allotter.allot(-0.4, 1) }).to.throw();
+        });
+    });
+
+    describe('with less than one as slots input', () => {
+        it('should throw an error', () => {
+            expect(() => { Allotter.allot(4, -4.565) }).to.throw();
         });
     });
 });
@@ -80,4 +102,39 @@ describe('Allotter allot()', () => {
             expect(totalPartsAllotted).to.eq(3);
         });
     })
+
+    describe('with 3 slots and 9 parts', () => {
+        it('should return 3 slots with 3 parts each', () => {
+            const partsPerSlot = Allotter.allot(9, 3);
+
+            expect(partsPerSlot[0]).to.eq(3);
+            expect(partsPerSlot[1]).to.eq(3);
+            expect(partsPerSlot[2]).to.eq(3);
+        });
+    })
+
+    describe('with 3 slots and 9 parts', () => {
+        it('total number of parts should be 9', () => {
+            const partsPerSlot = Allotter.allot(9, 3);
+
+            const totalPartsAllotted = partsPerSlot.reduce((acc, current) => {
+                return acc + current;
+            });
+
+            expect(totalPartsAllotted).to.eq(9);
+        });
+    })
+
+    describe('with 3 slots and 61 parts', () => {
+        it('total number of parts should be 61', () => {
+            const partsPerSlot = Allotter.allot(61, 3);
+
+            const totalPartsAllotted = partsPerSlot.reduce((acc, current) => {
+                return acc + current;
+            });
+
+            expect(totalPartsAllotted).to.eq(61);
+        });
+    })
+
 });
